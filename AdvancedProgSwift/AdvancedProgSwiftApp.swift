7cca2fb80185461f26f485e3bdf2e124fa1e2999
demenkoeugene <mimosa.01-purgers@icloud.com>
2023-05-18 16:30:47 +0300

    Initial Commit

---
+++ AdvancedProgSwift/AdvancedProgSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdvancedProgSwiftApp.swift
+//  AdvancedProgSwift
+//
+//  Created by Eugene Demenko on 18.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct AdvancedProgSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
