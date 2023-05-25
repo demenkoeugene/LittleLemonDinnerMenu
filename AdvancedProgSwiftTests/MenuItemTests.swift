77895af5f7d514b7a2094abbde34e284fdcab959
demenkoeugene <mimosa.01-purgers@icloud.com>
2023-05-25 17:41:53 +0300

    Add Test

---
+++ AdvancedProgSwiftTests/MenuItemTests.swift
@@ -0,0 +1,26 @@
+//
+//  AdvancedProgSwiftTests.swift
+//  AdvancedProgSwiftTests
+//
+//  Created by Eugene Demenko on 18.05.2023.
+//
+
+import XCTest
+@testable import AdvancedProgSwift // Import the module where MenuItem is defined
+
+class MenuItemTests: XCTestCase {
+    
+    func testMenuItemTitle() {
+        let title = "Burger"
+        let menuItem = MenuItem(price: 0.0, title: title, menuCategory: .food, ordersCount: 0, ingredient: [])
+        XCTAssertEqual(menuItem.title, title)
+    }
+
+    func testMenuItemIngredients() {
+        let ingredients: [Ingredient] = [.pasta, .spinach, .tomatoSauce]
+        let menuItem = MenuItem(price: 0.0, title: "Burger", menuCategory: .food, ordersCount: 0, ingredient: ingredients)
+        XCTAssertEqual(menuItem.ingredient, ingredients)
+    }
+
+}
+
